cd01be2fcfbb2a29d5ca19a3078fd1709cc7dac5
bjiang <bjiang@xogrp.com>
2016-03-10 12:09:30 +0800

    use gulp to minify js file

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # html5_features_detect
 A list of functions used to detect HTML5 featues
+## Usage
+```javascript
+var support = require('support');
+//support history pushState
+if(support.history) {
+  //your code 
+}
+```
+### html5 features detect
+1. location    
+2. input        
+3. placeholder  
+4. autofocus    
+5. history      
+6. canvas       
+7. video        
+8. localStorage 
+9. webworkers   
+10. offline      
---
+++ src/html5_detect.js
@@ -0,0 +1,95 @@
+// HTML5 features detection
+
+;(function(S) {
+  if (typeof module === 'object' && typeof require === 'function') {
+    module.exports.support = S;
+  } else {
+    this.support = S;
+  }
+})(function() {
+
+  /*** detect geolocation ****/
+
+  function support_geolocation() {
+    return 'geolocation' in navigator;
+  }
+
+
+  /*** detect input features ***/
+
+  function detect_input_features() {
+    var input = document.createElement('input');
+    input.setAttribute('type', 'password');
+    return input.type !== 'text';
+  }
+
+  /*** detect placeholder ***/
+
+  function detect_placeholder() {
+    var input = document.createElement('input');
+    return 'placeholder' in input;
+  }
+
+  /*** detect autofocus ***/
+
+  function detect_autofocus() {
+    var input = document.createElement('input');
+    return 'autofocus' in input;
+  }
+
+  /*** detect history state ***/
+
+  function supports_history_api() {
+    return !!(window.history && history.pushState);
+  }
+
+  /*** detect canvas ***/
+
+  function detect_canvas() {
+    return !!document.createElement('canvas').getContext;
+  }
+
+  /*** detect vedio ***/
+
+  function support_video() {
+    return !!document.createElement('video').canPlayType;
+  }
+
+  /*** support localstorage ***/
+
+  function support_localStorage() {
+    try {
+      return 'localStorage' in window && window['localStorage'] !== null;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  /*** support webworkers ***/
+
+  function support_webworkers() {
+    return !!window.Workers;
+  }
+
+  /*** support offline ***/
+
+  function supports_offline() {
+    return !!window.applicationCache;
+  }
+
+  return function() {
+    var support = {
+      location     : support_geolocation,
+      input        : detect_input_features,
+      placeholder  : detect_placeholder,
+      autofocus    : detect_autofocus,
+      history      : supports_history_api,
+      canvas       : detect_canvas,
+      video        : support_video,
+      localStorage : support_localStorage,
+      webworkers   :  support_webworkers,
+      offline      : supports_offline
+    };
+    return support;
+  }
+}());
